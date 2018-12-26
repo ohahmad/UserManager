ffed3d6ce1131ec4c2980ee4bc1424a67293ca6a
ommer ahmad <ommerahmad@hotmail.com>
2018-12-26 11:10:42 +0000

    added some extra fields to form

--- src/createUser/CreateUserForm.tsx
+++ src/createUser/CreateUserForm.tsx
@@ -21,41 +21,94 @@ export default class CreateUserForm extends Component<{}, IUser> {
         };    
     }
 
-    handleChange(event: React.FormEvent<HTMLInputElement>, fieldName: FieldName) {
+    handleInput(event: React.FormEvent<HTMLInputElement | HTMLSelectElement>, fieldName: FieldName) {
+        const value = event.currentTarget.value;
         switch(fieldName) {
             case FieldName.FirstName: {
                 this.setState({
-                    FirstName: event.currentTarget.value
+                    FirstName: value
+                });
+                break;
+            }
+            case FieldName.Surname: {
+                this.setState({
+                    Surname: value
+                });
+                break;
+            }
+            case FieldName.Age: {
+                this.setState({
+                    Age: parseInt(value)
+                });
+                break;
+            }
+            case FieldName.Gender: {
+                this.setState({
+                    Gender: (Gender as any)[value]
                 });
                 break;
             }
             case FieldName.RepositoryLink: {
                 this.setState({
-                    RepositoryLink: event.currentTarget.value
+                    RepositoryLink: value
                 });
                 break;
             }
         }
     }
 
-    render() {        
+    public isValid() : boolean {         
+         return this.state.FirstName !== "" 
+            && this.state.Surname !== ""
+            && this.state.Age > 0
+            && this.state.RepositoryLink != "";
+    }
+
+    public handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        debugger;
+        event.preventDefault();
+        console.log("submit clicked");
+        if(this.isValid()) {
+            // save store and eventually persist DB / storage
+        }
+        return false;
+    }
+    
+    render() { 
         return(
             <div className="creaeUserForm_container">
                 <h2>Create a new user</h2>
                 <p>Please fill in the details below and click 'Submit' to become a new user on this site. Becoming a member
-                    of this site gives you absolutely no benefit. So, what are you waiting for!
+        handleClickof this site gives you absolutely no benefit. So, what are you waiting for!
                 </p>
 
-                <form>
+                <form onSubmit={(event) => this.handleSubmit(event)}>
                     <div className="createUserForm_field">
                         <label className="createUserForm_fieldLabel">First Name: </label>               
-                        <input className="createUserForm_fieldInput" type = "text" maxLength = {15} value={this.state.FirstName} onChange = { (event) => this.handleChange(event, FieldName.FirstName)} />
+                        <input className="createUserForm_fieldInput" type = "text" maxLength = {15} value={this.state.FirstName} onChange = { (event) => this.handleInput(event, FieldName.FirstName)} />
+                    </div>
+                    <div className="createUserForm_field">
+                        <label className="createUserForm_fieldLabel">Surname: </label>               
+                        <input className="createUserForm_fieldInput" type = "text" maxLength = {15} value={this.state.Surname} onChange = { (event) => this.handleInput(event, FieldName.Surname)} />
+                    </div>
+                    <div className="createUserForm_field">
+                        <label className="createUserForm_fieldLabel">Age: </label>               
+                        <input className="createUserForm_fieldInput" type = "number" maxLength = {3} value={this.state.Age} onChange = { (event) => this.handleInput(event, FieldName.Age)} />
+                    </div>
+                    <div className="createUserForm_field">
+                        <label className="createUserForm_fieldLabel">Gender: </label>               
+                       <select className="createUserForm_fieldInput" onChange= { event => { this.handleInput(event, FieldName.Gender) } }>
+                            <option value="PreferNotToSay">Prefer Not To Say</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>    
                     </div>
                     <div className="createUserForm_field">
                         <label className="createUserForm_fieldLabel">Repository Link:</label>
-                        <input className="createUserForm_fieldInput" type = "text" placeholder = "You can enter a url or start typing the name of the user " value={this.state.RepositoryLink} onChange = { (event) => this.handleChange(event, FieldName.RepositoryLink)} />
+                        <input className="createUserForm_fieldInput" type = "text" placeholder = "You can enter a url or start typing the name of the user" value={this.state.RepositoryLink} onChange = { (event) => this.handleInput(event, FieldName.RepositoryLink)} />
                     </div>
                     <SearchGitHub searchTerm = {this.state.RepositoryLink} onRepositoryUrlSelected = { (url) => this.setState({ RepositoryLink: url }) }></SearchGitHub>
+                    <input type="submit" value="Create User" className="createUserForm_button"></input>
                 </form>
             </div>
         );
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,14 +1,9 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// global.fetch = require('jest-fetch-mock')
-// configure({ adapter: new Adapter() });
-
-
 import {GlobalWithFetchMock} from "jest-fetch-mock";
  
 const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
 customGlobal.fetch = require('jest-fetch-mock');
 customGlobal.fetchMock = customGlobal.fetch;
-
 
 configure({ adapter: new Adapter() });
